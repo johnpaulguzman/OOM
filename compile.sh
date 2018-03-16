ce9e867b690f4e4789ec7bdee1ca06923a6414f8
Your Name <you@example.com>
2018-03-16 21:01:19 +0800

    revise

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@ filename='main_math'
 nohuptime=1
 
 printf "\033c"  # clear screen
-for run in {1..2}; do
-  pdflatex -halt-on-error -interaction=batchmode ${filename}.tex
-  echo ">> Compilation attempt ${run}."
-done
+#for run in {1..2}; do
+pdflatex -halt-on-error -interaction=batchmode ${filename}.tex
+#echo ">> Compilation attempt ${run}."
+#done
 nohup evince ${filename}.pdf &
 sleep ${nohuptime}

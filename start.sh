326d77456f4783aa8d998e767864d09580f46e38
Your Name <you@example.com>
2018-03-28 17:41:48 +0800

    tourlakis

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #bookname='[Mendelson,_Elliott]_Introduction_to_mathematical_(b-ok.org)'
-bookname='[Dover books on mathematics] Stephen Cole Kleene - Mathematical Logic (2002, Dover Publications).pdf'
+bookname='[Dover books on mathematics] Stephen Cole Kleene - Mathematical Logic (2002, Dover Publications)'
 filename='main_math'
 nohuptime=1
 
-nohup subl ${filename}.tex &
-nohup evince ${filename}.pdf &
-nohup evince ${bookname}.pdf &
+nohup subl "${filename}.tex" &
+nohup evince "${filename}.pdf" &
+nohup evince "${bookname}.pdf" &
 sleep ${nohuptime}
